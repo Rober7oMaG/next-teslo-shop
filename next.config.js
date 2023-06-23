ffcf77db41d00a149d4311fbfb2a5d3d19bfa8fd
Roberto Macias <robertomag7@hotmail.com>
2023-06-22 18:07:28 -0600

    Configured all sources allowed

--- next.config.js
+++ next.config.js
@@ -3,11 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 
   async headers() {
-
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        source: "/",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
